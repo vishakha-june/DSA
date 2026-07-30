52a0c30fa078a0d6ed2791995f53554af425cd3b
Vishakha <148704436+vishakha-june@users.noreply.github.com>
2026-07-30 12:21:54 +0530

    Enhance comments for binary search explanations
    Added comments to clarify binary search logic for insert position and square root calculations.

--- Binary-Search/sqrt(x).java
+++ Binary-Search/sqrt(x).java
@@ -16,3 +16,32 @@ class Solution {
         
     }
 }
+
+// Here's the easiest sentence to remember.
+// Search Insert Position
+
+// Start points to the first place where the target CAN be inserted.
+
+// So
+
+// return start;
+// Sqrt(x)
+
+// End points to the largest number whose square is still valid.
+
+// So
+
+// return end;
+
+// ⭐ This is how I remember it.
+
+// Whenever Binary Search ends:
+
+// end < start
+
+// Then ask yourself:
+
+// What does the problem want?
+
+// Wants where to put something? 👉 Return start.
+// Wants the last valid answer? 👉 Return end.
